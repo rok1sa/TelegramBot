fbcf830122694fa4956ee6eb8009b8cdcf6d4038
rok1sa <139523036+rok1sa@users.noreply.github.com>
2024-03-19 20:52:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ print(sys.path)
 
 app = Flask(__name__)
 #app = Flask(__name__, template_folder='templates')
-CORS(app, origins='*')
+CORS(app, origins='https://telegrambot-krvu.onrender.com/')
 
 # MySQL setup
 db_config = {
